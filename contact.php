492c7bd233722a34f357811f4e4dc2e4631c2312
presidenhashfi <hnhgaming17@gmail.com>
2025-05-09 01:08:35 +0700

    update privacy

--- contact.php
+++ contact.php
@@ -1,8 +1,13 @@
 <?php
-$name = $_POST['name'];
-$email = $_POST['email'];
+// Ambil data dari form
+$name    = $_POST['name'];
+$email   = $_POST['email'];
 $message = $_POST['message'];
 
+// Load konfigurasi email
+$config = require 'config.php';
+
+// Include PHPMailer
 require 'PHPMailer-master/src/PHPMailer.php';
 require 'PHPMailer-master/src/SMTP.php';
 require 'PHPMailer-master/src/Exception.php';
@@ -13,28 +18,26 @@ use PHPMailer\PHPMailer\Exception;
 $mail = new PHPMailer(true);
 
 try {
-    $mail->SMTPDebug = 2;
-    $mail->Debugoutput = 'html';
-
     $mail->isSMTP();
-    $mail->Host = 'smtp.gmail.com';
-    $mail->SMTPAuth = true;
-    $mail->Username = 'haxovicastudios@gmail.com';
-    $mail->Password = 'sensor'; 
-    $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
-    $mail->Port = 587;
+    $mail->Host       = $config['smtp_host'];
+    $mail->SMTPAuth   = true;
+    $mail->Username   = $config['smtp_username'];
+    $mail->Password   = $config['smtp_password'];
+    $mail->SMTPSecure = $config['smtp_secure'];
+    $mail->Port       = $config['smtp_port'];
 
-    $mail->setFrom('haxovicastudios@gmail.com', 'Haxovica Studios');
-    $mail->addAddress('haxovicastudios@gmail.com');
+    // Set pengirim & penerima
+    $mail->setFrom($config['smtp_username'], 'Haxovica Studios');
+    $mail->addAddress($config['smtp_username']);
     $mail->addReplyTo($email, $name);
 
+    // Konten email
     $mail->isHTML(true);
     $mail->Subject = "Pesan dari $name";
-    $mail->Body = "Nama: $name<br>Email: $email<br>Pesan:<br>$message";
+    $mail->Body    = "Nama: $name<br>Email: $email<br>Pesan:<br>$message";
 
     $mail->send();
     header("Location: index.php?status=Pesan berhasil dikirim!");
 } catch (Exception $e) {
     echo "Pesan tidak dapat dikirim. Error: {$mail->ErrorInfo}";
-    exit();
 }
